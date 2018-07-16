257681e5d790628093530d7067874678e17cd3eb
John Salzman <john.r.salzman@gmail.com>
2018-07-16 19:31:17 -0400

    updated footer with link to home and new flex direction

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
 import newFlag from '../img/New-flag.gif';
 
@@ -7,8 +8,10 @@ import '../scss/footer.scss'
 const Footer = () => {
   return (
     <div id="footer">
-      <img src={newFlag} alt="Epsilon Omicron Flag" />
-      <a href="https://thetachiburg-original.netlify.com/" target="_blank">Duck's Original Site</a>
+      <Link to="/">
+        <img id="header-image" src={newFlag} alt="Epsilon Omicron Flag" />
+      </Link>
+      <a id="original-link" href="https://thetachiburg-original.netlify.com/" target="_blank">Duck's Original Site</a>
     </div>
   )
 }
